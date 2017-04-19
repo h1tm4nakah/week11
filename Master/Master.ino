dfa929b8dcf160cacb95c789169193e00d782d4b
h1tm4nakah <p.rustici93@gmail.com>
2017-04-19 09:46:28 +0200

    added Platform lib

--- Master/Master.ino
+++ Master/Master.ino
@@ -1,5 +1,5 @@
 #include <Wire.h>
-#include "Pump.h"
+#include "Platform.h"
 
 #define DRIVER_ENABLE 5
 #define DRIVER_STEP 6
@@ -9,7 +9,10 @@
 #define SERVO_2 10
 #define THERMISTOR A1
 
-Pump pump(DRIVER_ENABLE,DRIVER_STEP,DRIVER_DIR);
+#define MAX_TUBES 18
+#define STEPS_PER_TUBE 30
+
+Platform platform(DRIVER_ENABLE,DRIVER_STEP,DRIVER_DIR, MAX_TUBES, STEPS_PER_TUBE);
 
 int tube_number = 0;
 int pump_one = 0;
@@ -17,23 +20,41 @@ int pump_two;
 
 void setup() {
   Serial.begin(9600);
+  Wire.begin();
 }
 
 void loop() {
-  digitalWrite(LED,HIGH);
-  pump.flow(100);
-  digitalWrite(LED,LOW);
-
   while (Serial.available() > 0) {
     int tube_number = Serial.parseInt();
     int pump_one = Serial.parseInt();
     int pump_two = Serial.parseInt();
     if (Serial.read() == '\n') {
-        digitalWrite(LED, HIGH);
-        Serial.println('OK');
-        delay(3000);
-        digitalWrite(LED, LOW);
+        platform.goTo(tube_number);
+        blink_led(1);
+        delay(500);
+        Wire.beginTransmission(10);  // transmit to pump number 1
+        Wire.write(pump_one);       // send the % to flow
+        Wire.endTransmission();     // stop transmitting
+        blink_led(2);
+        delay(500);
+        Wire.beginTransmission(20);  // transmit to pump number 2
+        Wire.write(pump_two);       // send the % to flow
+        Wire.endTransmission();     // stop transmitting
+        blink_led(2);
+        delay(500);
+        Serial.println(1);          // send the ACK back to the computer
     }
   }
-  
 }
+
+void blink_led(int numb) {
+  for (int i=0; i<numb ; i++)
+  {
+      digitalWrite(LED, HIGH);
+      delay(200);
+      digitalWrite(LED, LOW);
+      delay(200);
+  }
+}
+
+
